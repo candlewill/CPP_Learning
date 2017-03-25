622ac17bdcf64706eb50a9da1a7d350dcbc79e0c
DESKTOP-KP0GSC5\heyunchao <yunchaohe@gmail.com>
2017-03-25 18:18:56 +0800

    raw demo

--- CPP_CMake_Projects/3_多个目录_多个源文件/main.cpp
+++ CPP_CMake_Projects/3_多个目录_多个源文件/main.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include<stdlib.h>
 
-#include"functions.h"
+#include"math/functions.h"
 
 int main(int argc, char* argv[])
 {
@@ -15,18 +15,18 @@ int main(int argc, char* argv[])
 	double result = power(base, exponent);
 	printf("%g ^ %d = %g\n", base, exponent, result);
 	return 0;
-/*
-%c	以字元方式輸出
-%d	10 進位整數輸出
-%o	以 8 進位整數方式輸出
-%u	無號整數輸出
-%x, %X	將整數以 16 進位方式輸出
-%f	浮點數輸出
-%e, %E	使用科學記號顯示浮點數
-%g, %G	浮點數輸出，取 %f 或 %e（%f 或 %E），看哪個表示精簡
-%%	顯示 %
-%s	字串輸出
-%lu	long unsigned 型態的整數
-%p	指標型態
-*/
+	/*
+	%c	以字元方式輸出
+	%d	10 進位整數輸出
+	%o	以 8 進位整數方式輸出
+	%u	無號整數輸出
+	%x, %X	將整數以 16 進位方式輸出
+	%f	浮點數輸出
+	%e, %E	使用科學記號顯示浮點數
+	%g, %G	浮點數輸出，取 %f 或 %e（%f 或 %E），看哪個表示精簡
+	%%	顯示 %
+	%s	字串輸出
+	%lu	long unsigned 型態的整數
+	%p	指標型態
+	*/
 }
